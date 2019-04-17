e9ddd12ef9c5f72e03d783ab76f48c4a13881e19
zx490336534 <490336534@qq.com>
2019-04-17 19:19:47 +0800

    UIView内部包含生命周期的冲载

--- SampleApp/SampleApp/ViewController.m
+++ SampleApp/SampleApp/ViewController.m
@@ -7,6 +7,36 @@
 //
 
 #import "ViewController.h"
+
+@interface TestView : UIView // 创建一个自己的TestView，继承于UIView
+
+
+@end
+
+@implementation TestView
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview{
+    [super willMoveToSuperview:newSuperview];
+}
+- (void)didMoveToSuperview{
+    [super didMoveToSuperview];
+}
+- (void)willMoveToWindow:(nullable UIWindow *)newWindow{
+    [super willMoveToWindow:newWindow];
+}
+- (void)didMoveToWindow{
+    [super didMoveToWindow];
+}
+
+@end
+
+
 @interface ViewController ()
 @end
 @implementation ViewController
@@ -14,10 +44,10 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     
-    UIView *view = [[UIView alloc] init];
-    view.backgroundColor = [UIColor redColor];
-    view.frame = CGRectMake(100, 100, 100, 100);
-    [self.view addSubview:view];
+//    UIView *view = [[UIView alloc] init];
+//    view.backgroundColor = [UIColor redColor];
+//    view.frame = CGRectMake(100, 100, 100, 100);
+//    [self.view addSubview:view];
     
     UIView *view2 = [[UIView alloc] init];
     view2.backgroundColor = [UIColor greenColor];
